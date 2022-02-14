fa942f223a468eb490aac56820f029952e1e4a02
Lakshmi Balasubramaniam <lrb697@gmail.com>
2022-02-14 12:42:19 -0500

    Added Vendor Class

--- PrsLibrary/Models/PrsDbContext.cs
+++ PrsLibrary/Models/PrsDbContext.cs
@@ -11,6 +11,7 @@ namespace PrsLibrary.Models {
     public class PrsDbContext : DbContext {
         
         public virtual DbSet<User> Users { get; set; } // Adding Class so will show up in the migration
+        public virtual DbSet<Vendor> Vendors { get; set; }
 
         // Constructors
         public PrsDbContext() { } // Default Constructor with no parameters
@@ -29,6 +30,11 @@ namespace PrsLibrary.Models {
             // Makes Username in User unique
             builder.Entity<User>(e => { // User is the name of Class
                 e.HasIndex(p => p.Username).IsUnique(true); // Column that want a index on is Username
+            });
+
+            // Makes Code column unique
+            builder.Entity<Vendor>(e => {
+                e.HasIndex(p => p.Code).IsUnique(true);
             });
         }
     }
