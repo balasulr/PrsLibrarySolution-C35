f94b53674ba42c95057651eb9076006ef5c58ff9
Lakshmi Balasubramaniam <lrb697@gmail.com>
2022-02-14 12:06:16 -0500

    Add project files.

---
+++ PrsLibrary/Models/PrsDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrsLibrary.Models {
+
+    public class PrsDbContext : DbContext {
+        
+        public virtual DbSet<User> Users { get; set; } // Adding Class so will show up in the migration
+
+        // Constructors
+        public PrsDbContext() { } // Default Constructor with no parameters
+        public PrsDbContext(DbContextOptions<PrsDbContext> options) : base(options) { }
+
+        //Methods
+        protected override void OnConfiguring(DbContextOptionsBuilder builder) {
+            if(!builder.IsConfigured) {
+                builder.UseSqlServer(
+                    "server=localhost\\sqlexpress;database=TestPrsDb;trusted_connection = true;"
+               );
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+            // Makes Username in User unique
+            builder.Entity<User>(e => { // User is the name of Class
+                e.HasIndex(p => p.Username).IsUnique(true); // Column that want a index on is Username
+            });
+        }
+    }
+}
---
+++ PrsLibrary/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrsLibrary.Models {
+
+    public class User {
+        // Adding Properties to class
+        public int Id { get; set; }
+        [Required,StringLength(30)] // Attributes combined in one line & makes sure defined properly in SQL
+        public string Username { get; set; }
+        [Required, StringLength(30)] // Sets the Password having the Null be No so is not allowed to be null/ not null
+        public string Password { get; set; }
+        [Required, StringLength(30)]
+        public string Firstname { get; set; }
+        [Required, StringLength(30)]
+        public string Lastname { get; set; }
+        [StringLength(12)] // Phone is Yes for null and same for Email
+        public string Phone { get; set; }
+        [StringLength(255)]
+        public string Email { get; set; }
+        public bool IsReviewer { get; set; }
+        public bool IsAdmin { get; set; }
+
+        // Adding a default constructor on one line to save space
+        public User() { }
+    }
+}
