bfb55f30bdaa12a7a9c0da73fa25114178e811fb
Lakshmi Balasubramaniam <lrb697@gmail.com>
2022-02-14 16:40:55 -0500

    Added User Controller

---
+++ PrsLibrary/Controllers/UsersController.cs
@@ -0,0 +1,59 @@
+﻿using PrsLibrary.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrsLibrary.Controllers {
+
+    public class UsersController {
+        // Private Property for Context
+        private readonly PrsDbContext _context; // readonly is a safetynet. Only way that can set is in the contructor
+
+        public UsersController(PrsDbContext context) { // Constructor
+            this._context = context;
+        }
+
+        // 2 Read fuctions
+        // Method to return all of the users in the User Table
+        public IEnumerable<User> GetAll() {
+            return _context.Users.ToList(); // Returns all users in a List
+        }
+
+        // Allow user to read by primary key (Most that can read is 1)
+        // Returns the user instance or null
+        public User GetByPk(int id) {
+            return _context.Users.Find(id);
+        }
+
+        // Insert Statement
+        public User Create(User user) {
+            if(user is null) {
+                throw new ArgumentNullException("user");
+            }
+            if(user.Id != 0) {
+                throw new ArgumentException("User.Id must be zero!");
+            }
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return user;
+        }
+
+        // Update - Change some of the data for a user
+        public void Change(User user) {
+            _context.SaveChanges();
+        }
+
+        // Delete where the user passes in the primary key
+        public void Remove(int id) {
+            var user = _context.Users.Find(id);
+            if(user is null) {
+                throw new Exception("User not found!");
+            }
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+        }
+    }
+}
--- TestPrsLibrary/Program.cs
+++ TestPrsLibrary/Program.cs
@@ -1,9 +1,55 @@
-﻿using System;
+﻿using PrsLibrary.Controllers;
+using PrsLibrary.Models;
+
+using System;
 
 namespace TestPrsLibrary {
+    
     class Program {
+        
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            var context = new PrsDbContext();
+
+            // Instance of the controller
+            var userCtrl = new UsersController(context);
+
+            // Calls the Create method to add a User
+            var newUser = new User() {
+                Id = 0, Username = "zz", Password = "xx",
+                Firstname = "User", Lastname = "ZZ",
+                Phone = "211", Email = "xx@user.com",
+                IsReviewer = false, IsAdmin = false
+            };
+
+            //userCtrl.Create(newUser); //// This line adds a user
+
+            var user3 = userCtrl.GetByPk(3);
+            if(user3 is null) {
+                Console.WriteLine("User not found!");
+            } else {
+                Console.WriteLine($"User 3: {user3.Firstname} {user3.Lastname}");
+            }
+
+            user3.Lastname = "User3";
+            userCtrl.Change(user3);
+            ///////////////////////////////////////////////////////////////////////////////
+            var user33 = userCtrl.GetByPk(33);
+            if (user33 is null) {
+                Console.WriteLine("User not found!");
+            } else {
+                Console.WriteLine($"User33: {user3.Firstname} {user3.Lastname}");
+            }
+            ///////////////////////////////////////////////////////////////////////////////
+
+            //userCtrl.Remove(6);
+
+            // Methods to get all users
+            var users = userCtrl.GetAll();
+
+            foreach(var user in users) {
+                Console.WriteLine($"{user.Id} {user.Firstname} {user.Lastname}");
+            }
         }
     }
 }
